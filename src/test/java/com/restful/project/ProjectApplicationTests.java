c5649139142f8bd8a320aa0ad0b51ae789654f06
Ali <alisen4099@gmail.com>
2023-08-28 01:18:10 +0300

    first commit

---
+++ src/test/java/com/restful/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
